0111f277c7015cbcb266fdc9aa4cf55d5c81329c
Sergio Del Rio <sergio.delriodiaz@gmail.com>
2013-08-06 00:33:47 -0400

    Add User Model with Devise

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable,  :token_authenticatable #,
+         :validatable
+  #
+
+  attr_accessible :title, :body, :first_name, :last_name, :email,
+   :password, :password_confirmation, :name
+end
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Omrails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Requerido para Heroku
+    config.assets.initialize_on_precomplie = false
   end
 end
